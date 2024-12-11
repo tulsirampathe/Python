ec0fc831c896db685ac4c03f34839de8949f4569
tulsirampathe <tulsirampathe81@gmail.com>
2024-12-11 23:36:12 +0530

    chapter 1

---
+++ Chapter 1/module.py
@@ -0,0 +1,14 @@
+import pyjokes
+
+print("Printing Jokes...")
+
+# Single line commment
+# This prints a random joke
+joke = pyjokes.get_joke()
+print(joke)
+
+# Multiple line comments
+"""
+    So thanks 
+    that was my program
+    """
