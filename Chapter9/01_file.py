8361fd946f9bf5c866734fd2d4d6fed4e0aa43e5
tulsirampathe <tulsirampathe81@gmail.com>
2025-02-06 20:56:10 +0530

    Chapter 9

---
+++ Chapter9/01_file.py
@@ -0,0 +1,12 @@
+'''
+a = "a very long string with emils"
+
+emails = []
+3 seconds
+'''
+
+f = open("file.txt")
+data = f.read()
+
+print(data)
+f.close()
---
+++ Chapter9/02_file_write.py
@@ -0,0 +1,7 @@
+st = "Hey Pathe you are amazing"
+
+f = open("myfile.txt", "w")
+
+f.write(st)
+
+f.close()
